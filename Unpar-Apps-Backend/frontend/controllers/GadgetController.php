27f451eae6259808e971a82e855bc51c3039e2b3
nadyahw <nadyahw@outlook.com>
2016-06-14 22:32:06 +0700

    erased everything again

--- Unpar-Apps-Backend/frontend/controllers/GadgetController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace frontend\controllers;
-
-use yii\rest\ActiveController;
-
-class GadgetController extends ActiveController{
-    public $modelClass = 'common\models\Gadget';
-}
--- Unpar-Apps-Backend/frontend/controllers/v3/GadgetController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace frontend\controllers\v3;
-
-use yii\rest\ActiveController;
-
-class GadgetController extends ActiveController{
-    public $modelClass = 'common\models\Gadget';
-}
