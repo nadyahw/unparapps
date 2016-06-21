96a987518d63957a79952c318eac15eaf5c3cf18
nadyahw <nadyahw@outlook.com>
2016-06-21 22:06:41 +0700

    erased all gitignore

---
+++ Unpar-Apps-Backend/backend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'sx9RJEl16_9y6zn33mwfcnFXXav3h75q',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
